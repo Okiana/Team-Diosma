4a38b735005808a8e9e873a2ddb0d4a08754faa8
Daniel-Georgiev <daniko91@mail.bg>
2015-07-25 23:57:33 +0300

    sqlite sample datebase

---
+++ MarketSystem/MarketSystem.SQLite/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketSystem.SQLite
+{
+    public class Class1
+    {
+    }
+}
